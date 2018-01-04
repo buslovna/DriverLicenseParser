e36c1f2fb6c8df80e668ba0aa03057a1d10c1d5c
buslovna <buslovna@ufl.edu>
2018-01-03 19:56:16 -0500

    removing target

--- src/test/java/hello/GreetingControllerTests.java
+++ src/test/java/hello/GreetingControllerTests.java
@@ -15,10 +15,6 @@
  */
 package hello;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
